a44ed8595d8f6665e193ed2b2a15515e4c33204f
Miguelpellegrino <miguelpellegrino2019@gmail.com>
2021-04-05 09:54:01 -0400

    [ADD] Fuente para la imagen

--- code/create_img.py
+++ code/create_img.py
@@ -13,10 +13,9 @@ def crearimagen(autenticacion):
     if autenticacion == True:
         requestt = session.get(urls_lvl3[2],stream=True)
         image = requestt.raw
-        fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 20)
         image = Image.open(image)
         draw = ImageDraw.Draw(image)
-        draw.text((5,5), f"Miguel Armando Pellegrino Cardenas, \n miguelpellegrino2019@gmail.com \n Programador en Python",font=fnt ,fill=(255,255,255,128))
+        draw.text((5,5), f"Miguel Armando Pellegrino Cardenas, \n miguelpellegrino2019@gmail.com \n Programador en Python",fill=(255,255,255,128))
         image.save("image.jpg","JPEG")
     else:
         print("Fallo en la autenticacion de las URL")
